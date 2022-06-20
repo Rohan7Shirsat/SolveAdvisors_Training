8d317ca5ffbe2a65d79af10d908a78eb6901e31d
Rohan <98087223+Rohan7Shirsat@users.noreply.github.com>
2022-06-20 22:02:40 +0530

    Add files via upload

---
+++ FactoryPattern.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    interface Transport
+    {
+        public void Transportation();
+    }
+
+    class Tempo : Transport
+    {
+        public void Transportation()
+        {
+            Console.WriteLine("Transportation By Tempo");
+        }
+    }
+
+    class Ship : Transport
+    {
+        public void Transportation()
+        {
+            Console.WriteLine("Transportation By ship");
+        }
+    }
+
+    class FactoryClass
+    {
+        public static Transport GetTransportationType(String s)
+        {
+            if (s == null)
+            {
+                return null;
+            }
+
+            if (s == "Tempo")
+            {
+                return new Tempo();
+            }
+            else if (s == "Ship")
+            {
+                return new Ship();
+            }
+            else
+                return null;
+        }
+    }
+
+    class DriverClass
+    {
+        public static void Main(string[] args)
+        {
+           
+            Transport Tempo = FactoryClass.GetTransportationType("Tempo");
+            Tempo.Transportation();
+
+            Transport Ship = FactoryClass.GetTransportationType("Ship");
+            Ship.Transportation();
+        }
+
+    }
+
+}
---
+++ SingltonWithoutLock.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SingltonWithoutLock
+{
+    sealed class Settelite
+    {
+        private static Settelite Instance = null;
+        private Settelite()
+        {
+            Console.WriteLine("Constructor called");
+        }
+      
+        public static Settelite getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new Settelite();
+                return Instance;
+            }
+        }
+
+        public void RunThePicture()
+        {
+            Console.WriteLine("Picture is Running .... (Time - 1:45:50)");
+          
+        }
+    }
+
+    class CinemaHall
+    {
+        public static void Main(String[] args)
+        {
+            Settelite CinemaHall1 = Settelite.getInstance;
+            CinemaHall1.RunThePicture();
+
+            Settelite CinemaHall2= Settelite.getInstance;
+            CinemaHall2.RunThePicture();
+        }
+
+
+    }
+}
+
