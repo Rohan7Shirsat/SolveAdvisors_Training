c448b2aa4a972e6f972d4385cb7c968f190b15f7
Rohan <98087223+Rohan7Shirsat@users.noreply.github.com>
2022-06-21 12:21:46 +0530

    Add files via upload

---
+++ SingltonWithLock.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SingltonWithLock
+{
+    sealed class VotingMachine
+    {
+        private static VotingMachine Instance = null;
+        private VotingMachine()
+        {
+            Console.WriteLine("Constructor called");
+        }
+
+        public static VotingMachine getInstance
+        {
+            get
+            {
+                if (Instance == null)
+                    Instance = new VotingMachine();
+                return Instance;
+            }
+        }
+
+        public void DoVoting()
+        {
+            Console.WriteLine("Voter doing his/her voting");
+
+        }
+    }
+
+    class Voter
+    {
+        public static void Main(String[] args)
+        {
+            VotingMachine voter1 = VotingMachine.getInstance;
+            voter1.DoVoting();
+
+            VotingMachine voter2 = VotingMachine.getInstance;
+            voter2.DoVoting();
+        }
+
+
+    }
+}
+
